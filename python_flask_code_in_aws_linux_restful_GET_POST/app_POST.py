6b7bad50fb023906a971fe4ba7f5b1c82ed78765
vimallinuxworld13 <vdaga@lwindia.com>
2022-04-08 13:46:16 +0530

    Create app_POST.py

---
+++ python_flask_code_in_aws_linux_restful_GET_POST/app_POST.py
@@ -0,0 +1,14 @@
+from flask import request
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
