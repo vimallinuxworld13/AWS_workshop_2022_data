84e06949542bc50464405e12f30935bec5a9fdca
vimallinuxworld13 <vdaga@lwindia.com>
2022-04-08 13:42:29 +0530

    Create app_GET.py

---
+++ python_flask_code_in_aws_linux_restful_GET_POST/app_GET.py
@@ -0,0 +1,28 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
